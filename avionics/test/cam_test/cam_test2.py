cd56f3abd7663e55ba4683bf81c88937a8a17edb
millermcswain <millermcswain@gmail.com>
2020-03-13 22:27:27 +0000

    cam test

--- avionics/test/cam_test/cam_test2.py
+++ avionics/test/cam_test/cam_test2.py
@@ -3,5 +3,5 @@ import picamera
 camera = picamera.PiCamera()
 camera.resolution = (640, 480)
 camera.start_recording('my_video.h264')
-camera.wait_recording(60)
+camera.wait_recording(5)
 camera.stop_recording()
