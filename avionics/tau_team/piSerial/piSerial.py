7ab0ae1581ccb4069cc53d8c3cdb8bcbfd8fec6f
SanjnaR <sanjna.ravichandar@gmail.com>
2020-03-15 03:01:55 +0000

    pi-teensy comms

--- avionics/tau_team/piSerial/piSerial.py
+++ avionics/tau_team/piSerial/piSerial.py
@@ -1,12 +1,28 @@
 import serial
-import RPi.GPIO as GPIO
 import time
+
+def read_teensy():
+    read_ser=ser.readline()
+    print(read_ser)
+
+def write_teensy(msg):
+    msg=msg.encode()
+    ser.write(msg)
+
+def handshake():
+    com_connection=False
+    while not(com_connection):
+        
+        ser.write(b'start')
+        read_ser=ser.readline()
+        test=str(read_ser)
+        print(str(read_ser))
+        if len(read_ser)>0:
+            com_connection=True
+            print('good')
 
 port='/dev/ttyAMA0'
 baudrate=115200
 ser=serial.Serial(port, baudrate)  #change ACM number as found from ls /dev/tty/ACM*
-print(ser)
-while True:
-  read_ser=ser.readline()
-  print(read_ser)
-  time.sleep(1)
+
+handshake()
