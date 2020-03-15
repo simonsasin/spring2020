1479030ae79d836b4c99af519958d374fda81928
millermcswain <millermcswain@gmail.com>
2020-03-15 01:22:17 +0000

    pi-teensy comms

--- avionics/tau_team/piSerial/piSerial.py
+++ avionics/tau_team/piSerial/piSerial.py
@@ -1,32 +1,12 @@
-##################################ISP_Avionics#################################
-#        Name: pySerial.py
-#     Created: 3.13.20 by Blake Shaffer and Miller McSwain
-# Description:
-
 import serial
+import RPi.GPIO as GPIO
+import time
 
-baudrate = 115200  # set baudrate to 115200 bits/sec
-serial_port = "/dev/ttyS0"  # UART serial pins PL011
-
-
-###################################FUNCTIONS!##################################
-# -----------------------------------------------------------------------------
-# Description:
-#          In:
-#  Out/Modify:
-# -----------------------------------------------------------------------------
-ser = serial.Serial(serial_port, baudrate)
-
-
-i=0
+port='/dev/ttyAMA0'
+baudrate=115200
+ser=serial.Serial(port, baudrate)  #change ACM number as found from ls /dev/tty/ACM*
+print(ser)
 while True:
-    print(i)
-
-    ser.write(b"hi from pi")
-    read_ser=ser.readline()
-    print(read_ser)
-    i=i+1
-######################################MAIN#####################################
-
-
-##################################ISP_Avionics#################################
+  read_ser=ser.readline()
+  print(read_ser)
+  time.sleep(1)
